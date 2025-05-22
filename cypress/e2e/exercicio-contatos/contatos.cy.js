c58d0ef7ee9f1944dbdfdb951c056f012011dceb
Daniel Alves <daniel199831.da@gmail.com>
2025-05-22 19:38:42 -0300

    adicionando alteração

--- cypress/e2e/exercicio-contatos/contatos.cy.js
+++ cypress/e2e/exercicio-contatos/contatos.cy.js
@@ -6,25 +6,30 @@ describe('Testes para os contatos', () => {
     })
 
     it('Deve adicionar um contao', () => {
+        cy.get('input').should('have.length', 3)
         cy.get('[type="text"]').type('Daniel Alves')
         cy.get('[type="email"]').type('daniel199831.da@gmail.com')
         cy.get('[type="tel"]').type('(24)999999999')
         cy.get('.adicionar').click()
-    })
 
+        cy.screenshot('adicioando-contato')
+    })
+    
     it('Deve editar um contao', () => {
+        cy.get('input').should('have.length', 3)
         cy.get('.edit').first().click()
         cy.get('[type="text"]').type('Alves Alves')
         cy.get('[type="email"]').type('daniel199831.da@gmailcom')
         cy.get('[type="tel"]').type('(24)111111111')
         cy.get('.alterar').click()
+
+        cy.screenshot('aditando-contato')
     })
 
     it('Deve remover um contao', () => {
         cy.get('.delete').first().click()
-        cy.get('[type="text"]').type('Alves Alves')
-        cy.get('[type="email"]').type('daniel199831.da@gmailcom')
-        cy.get('[type="tel"]').type('(24)111111111')
+
+        cy.screenshot('removendo-contato')
     })
     
 })
