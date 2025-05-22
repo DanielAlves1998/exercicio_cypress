4bfbbbd92713afada10395ee873f9c97148cee5c
Daniel Alves <daniel199831.da@gmail.com>
2025-05-22 23:42:38 +0000

    adicionando verificação

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,12 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/relatorio',
+    overwrite: true, //aq é para se fizer mais de um teste o sistema não crie mais um arquivo
+    html: true,
+    json: false,
+    timestamp: 'ddmmyyyy__HHMMss'
+  }
 });
--- cypress/e2e/exercicio-contatos/home.cy.js
+++ cypress/e2e/exercicio-contatos/home.cy.js
@@ -8,8 +8,4 @@ describe('Testes para a home', () => {
     it('Deve renderizar 3 contato', () => {
         cy.get('.sc-iAEyYk jtXTMB > div').should('have.length', 0)
     })
-
-    it('Deve filtrar por gian Souza', () => {
-        cy.get('.sc-gLDzan ckeKmo').type('gian Souza{enter}')
-    })
 })
